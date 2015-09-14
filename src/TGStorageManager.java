99c0f2a4c016675824b787f891e5a432b4accca7
harryggg <matanghao1@matanghaodeMacBook-Pro.local>
2015-09-14 23:03:33 +0800

    FEATURE ADD:inititalise empty xml file when file not found

--- src/TGStorageManager.java
+++ src/TGStorageManager.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -9,6 +11,9 @@ import java.util.Date;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
@@ -34,24 +39,28 @@ public class TGStorageManager {
 		this._scheduleCache = new ArrayList<Event>();
 		initialize();
 	}
-	
-	public ArrayList<Event> getTaskCache(){
+
+	public ArrayList<Event> getTaskCache() {
 		return this._taskCache;
 	}
-	
-	public ArrayList<Event> getScheduleCache(){
+
+	public ArrayList<Event> getScheduleCache() {
 		return this._scheduleCache;
 	}
-	
-	public ArrayList<Event> getDeadlineCache(){
+
+	public ArrayList<Event> getDeadlineCache() {
 		return this._deadlineCache;
 	}
-	
+
 	private void initialize() {
 		try {
 			File inputFile = new File(_fileDirectory);
-			//TODO file existence
-			//System.out.println(inputFile.exists());
+			// TODO file existence
+			// System.out.println(inputFile.exists());
+			if (!inputFile.exists()) {
+				createStorageFile();
+				return;
+			}
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
 					.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -69,21 +78,57 @@ public class TGStorageManager {
 			e.printStackTrace();
 		}
 	}
-	
+
+	private void createStorageFile() {
+		StringWriter stringWriter = new StringWriter();
+
+		XMLOutputFactory xMLOutputFactory = XMLOutputFactory.newInstance();
+		XMLStreamWriter xMLStreamWriter;
+		try {
+			xMLStreamWriter = xMLOutputFactory
+					.createXMLStreamWriter(stringWriter);
+
+			xMLStreamWriter.writeStartDocument();
+			xMLStreamWriter.writeStartElement("calendar");
+			xMLStreamWriter.writeEndElement();
+	        xMLStreamWriter.writeEndDocument();
+
+	         xMLStreamWriter.flush();
+	         xMLStreamWriter.close();
+	         
+		} catch (XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		try {
+			FileWriter fw = new FileWriter(_fileDirectory);
+			fw.write(stringWriter.getBuffer().toString());
+			stringWriter.close();
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 	private void initializeTaskCache(Document doc) {
 
 		NodeList nodeList;
 		XPath xPath = XPathFactory.newInstance().newXPath();
 		try {
-			nodeList = (NodeList) xPath.compile(Constants.XML_TASK_EXPRESSION).evaluate(doc,
-					XPathConstants.NODESET);
+			nodeList = (NodeList) xPath.compile(Constants.XML_TASK_EXPRESSION)
+					.evaluate(doc, XPathConstants.NODESET);
 			for (int i = 0; i < nodeList.getLength(); i++) {
 				Node nNode = nodeList.item(i);
-				//System.out.println("\nCurrent Element :" + nNode.getNodeName());
+				// System.out.println("\nCurrent Element :" +
+				// nNode.getNodeName());
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element) nNode;
-					_taskCache.add(new Event(eElement.getElementsByTagName("name").item(0)
-									.getTextContent()));
+					_taskCache.add(new Event(eElement
+							.getElementsByTagName("name").item(0)
+							.getTextContent()));
 				}
 			}
 		} catch (XPathExpressionException e) {
@@ -91,25 +136,30 @@ public class TGStorageManager {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void initializeDeadlineCache(Document doc) {
 		DateFormat sdf = new SimpleDateFormat(Constants.DEFAULT_DATE_FORMAT);
 		NodeList nodeList;
 		XPath xPath = XPathFactory.newInstance().newXPath();
 		try {
-			nodeList = (NodeList) xPath.compile(Constants.XML_DEADLINE_EXPRESSION).evaluate(doc,
+			nodeList = (NodeList) xPath.compile(
+					Constants.XML_DEADLINE_EXPRESSION).evaluate(doc,
 					XPathConstants.NODESET);
-			String nameString,startDateString;
+			String nameString, startDateString;
 			Date startDate;
 			for (int i = 0; i < nodeList.getLength(); i++) {
 				Node nNode = nodeList.item(i);
-				//System.out.println("\nCurrent Element :" + nNode.getNodeName());
+				// System.out.println("\nCurrent Element :" +
+				// nNode.getNodeName());
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element) nNode;
-					nameString = eElement.getElementsByTagName("name").item(0).getTextContent();
-					startDateString = eElement.getElementsByTagName("startDate").item(0).getTextContent();
+					nameString = eElement.getElementsByTagName("name").item(0)
+							.getTextContent();
+					startDateString = eElement
+							.getElementsByTagName("startDate").item(0)
+							.getTextContent();
 					startDate = sdf.parse(startDateString);
-					_deadlineCache.add(new Event(nameString,startDate));
+					_deadlineCache.add(new Event(nameString, startDate));
 				}
 			}
 		} catch (XPathExpressionException e) {
@@ -120,26 +170,32 @@ public class TGStorageManager {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void initializeScheduleCache(Document doc) {
 		DateFormat sdf = new SimpleDateFormat(Constants.DEFAULT_DATE_FORMAT);
 		NodeList nodeList;
 		XPath xPath = XPathFactory.newInstance().newXPath();
 		try {
-			nodeList = (NodeList) xPath.compile(Constants.XML_SCHEDULE_EXPRESSION).evaluate(doc,
+			nodeList = (NodeList) xPath.compile(
+					Constants.XML_SCHEDULE_EXPRESSION).evaluate(doc,
 					XPathConstants.NODESET);
-			String nameString,startDateString,endDateString;
+			String nameString, startDateString, endDateString;
 			Date startDate, endDate;
 			for (int i = 0; i < nodeList.getLength(); i++) {
 				Node nNode = nodeList.item(i);
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element) nNode;
-					nameString = eElement.getElementsByTagName("name").item(0).getTextContent();
-					startDateString = eElement.getElementsByTagName("startDate").item(0).getTextContent();
-					endDateString = eElement.getElementsByTagName("endDate").item(0).getTextContent();
+					nameString = eElement.getElementsByTagName("name").item(0)
+							.getTextContent();
+					startDateString = eElement
+							.getElementsByTagName("startDate").item(0)
+							.getTextContent();
+					endDateString = eElement.getElementsByTagName("endDate")
+							.item(0).getTextContent();
 					startDate = sdf.parse(startDateString);
 					endDate = sdf.parse(endDateString);
-					_scheduleCache.add(new Event(nameString,startDate,endDate));
+					_scheduleCache
+							.add(new Event(nameString, startDate, endDate));
 				}
 			}
 		} catch (XPathExpressionException e) {
@@ -150,19 +206,21 @@ public class TGStorageManager {
 			e.printStackTrace();
 		}
 	}
+
 	public static void main(String[] args) {
-		TGStorageManager tm = new TGStorageManager("test");
-		for (Event element:tm.getTaskCache()){
+		TGStorageManager tm = new TGStorageManager("new");
+		for (Event element : tm.getTaskCache()) {
 			System.out.println(element.name);
 		}
 		System.out.println();
-		for (Event element:tm.getDeadlineCache()){
-			System.out.println(element.name+":"+element.startDate);	
+		for (Event element : tm.getDeadlineCache()) {
+			System.out.println(element.name + ":" + element.startDate);
 		}
 		System.out.println();
-		for (Event element:tm.getScheduleCache()){
-			System.out.println(element.name+":"+element.startDate+"-"+element.endDate);
-			
+		for (Event element : tm.getScheduleCache()) {
+			System.out.println(element.name + ":" + element.startDate + "-"
+					+ element.endDate);
+
 		}
 	}
 }
