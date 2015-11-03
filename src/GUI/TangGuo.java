ef0df76ba216facd088dc9d3f613c369920fe788
harryggg <ma.tanghao@dhs.sg>
2015-11-03 18:03:03 +0800

    disable row selection

--- src/GUI/TangGuo.java
+++ src/GUI/TangGuo.java
@@ -1,12 +1,21 @@
 package GUI;
 
 import javax.swing.JFrame;
-
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 public class TangGuo {
 	public static void main(String[] args) {
+		/*
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeels");
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 		JFrame frame = new MainFrame("TangGuo",args[0]);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800, 600);
+		frame.setSize(1000, 800);
 		frame.setVisible(true);
 
 
